fd20cf450bebcb1a784dd6f94f1a7cf1ce507713
julinha <juliarodriguesg2007@gmail.com>
2024-11-25 08:33:32 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,53 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Medicina',
+    'O que é um Estetoscópio?',
+    'O Estetoscópio é um equipamento médico que permite ouvir o coração e o pulmão dos pacientes.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Anatomia',
+    'Qual a quantidade de ossos do corpo humano?',
+    'Ao total são 206 Ossos.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Medicina',
+    'Qual a especialidade médica estudada para se cuidar do ouvido, nariz, seios paranasais, faringe, laringe, cabeça e pescoço?',
+    'A especialidade médica é a Otorrinolaringologista.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Anatomia',
+    'Qual é o maior orgão do corpo humano?',
+    'O maior orgão do corpo humano é a Pele, pois é responsável por recobrir toda nossa superfície corpórea.'
+)
+
+criaCartao(
+    'Medicina',
+    'Qual é o exame mais comum feito por um Cardiologista?',
+    'O exame mais comum é o Eletrocardiograma.'
+)
+
+criaCartao(
+    'Anatomia',
+    'A pele é formada por quais tecidos?',
+    'Ela é formada pelos tecidos Epitelial e Conjuntivo, sendo eles a Epiderme e a Derme.'
+)
+
+criaCartao(
+    'Medicina',
+    'Uretra é um orgão pertencente a qual sistema?',
+    'A Uretra faz parte do sistema Urinário, pois é um canal que conduz a urina da bexiga até o meio externo.'
+)
+
+criaCartao(
+    'Anatomia',
+    'O que é Anatomia?',
+    'A Anatomia é o campo da biologia responsável por estudar a forma e a estrutura do organismo humano.'
+)
+
+criaCartao(
+    'Medicina',
+    'Qual o orgão humano responsável pela detoxificação do Álcool no sangue?',
+    'O orgão responsável pela detoxificação é o Fígado, pois exerce diferentes funções em nosso corpo, sendo o responsável pela produção da Bile e atuando na eliminação de substâncias tóxicas..'
 )
